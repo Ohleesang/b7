bb2c4f719fe109a53851a85f4f190978a607b97a
Ohleesang <jammmmed@gmail.com>
2023-12-27 10:15:04 +0900

    간단한 계산문제

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,13 @@
 class Solution {
+    fun solution(n: Int, k: Int): Int {
+        var answer: Int = 0
+        var service = n/10
+        answer = n*12000+(k-service)*2000
+        return answer
+    }
+}
+fun main(){
+    val a = Solution()
+    a.solution(10,3)//124,000
+    a.solution(64,6)//768,000
 }
